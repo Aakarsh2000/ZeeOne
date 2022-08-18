3757e73cf1d84b5645436252123e10b50f56aa09
Aakarsh2000 <saiaakarshp@gmail.com>
2022-08-18 16:01:03 +0530

    Added Student.java file

---
+++ Student.java
@@ -0,0 +1,16 @@
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Setter
+@Getter
+@ToString
+public class Student 
+{
+	private int usn;
+	private String name,branch;
+	
+
+}
